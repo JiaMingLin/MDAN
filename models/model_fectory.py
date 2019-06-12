e325bb42fb2593059123b48ec759263a327699e1
JiaMingLin <robin890650@gmail.com>
2019-06-12 16:37:05 +0800

    add senet_extractor

--- models/model_fectory.py
+++ models/model_fectory.py
@@ -5,6 +5,7 @@ from torchvision import datasets, models, transforms
 from .alexnet_extractor import alexnet_extractor
 from .vgg16_extractor import vgg16_extractor
 from .resnet50_extractor import resnet50_extractor
+from .senet_extractor import senet154, se_resnet50, se_resnet101, se_resnet152
 
 class GradientReversalLayer(torch.autograd.Function):
     """
@@ -92,8 +93,13 @@ class MDANet(nn.Module):
 extractor_mapping = {
     'alexnet': alexnet_extractor(pretrained=True),
     'vgg16_bn': vgg16_extractor(pretrained=True),
-    'resnet50': resnet50_extractor(pretrained=True)
+    'resnet50': resnet50_extractor(pretrained=True),
+    'senet154': senet154(pretrained=True), 
+    'se_resnet50': se_resnet50(pretrained=True), 
+    'se_resnet101': se_resnet101(pretrained=True), 
+    'se_resnet152': se_resnet152(pretrained=True)
 }
+
 def load_model(name, class_num, domain_num, extractor = 'alexnet'):
     feature_extractor = extractor_mapping[extractor]
     if name == 'mdan':
