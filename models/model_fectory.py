71607f895edfc52f2cbe7f11bd2aa23cbd94cbb7
JiaMingLin <robin890650@gmail.com>
2019-06-12 17:40:28 +0800

    add se-resnext

--- models/model_fectory.py
+++ models/model_fectory.py
@@ -97,7 +97,9 @@ extractor_mapping = {
     'senet154': senet154(pretrained=True), 
     'se_resnet50': se_resnet50(pretrained=True), 
     'se_resnet101': se_resnet101(pretrained=True), 
-    'se_resnet152': se_resnet152(pretrained=True)
+    'se_resnet152': se_resnet152(pretrained=True),
+    'se_resnext50_32x4d': se_resnext50_32x4d(pretrained=True),
+    'se_resnext101_32x4d': se_resnext101_32x4d(pretrained=True)
 }
 
 def load_model(name, class_num, domain_num, extractor = 'alexnet'):
